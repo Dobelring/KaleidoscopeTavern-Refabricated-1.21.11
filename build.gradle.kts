fcaac42e20f520a22822fcf3c447a77652982116
NightEpiphany <nightepiphany0401@gmail.com>
2026-06-23 00:39:28 +0800

    REI兼容

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,8 @@ repositories {
 		name = "Nucleoid"
 		url = URI("https://maven.nucleoid.xyz/releases")
 	}
+
+	maven { url = URI("https://maven.shedaniel.me") }
 }
 
 dependencies {
@@ -48,6 +50,10 @@ dependencies {
 	implementation ("maven.modrinth:create-fly:${providers.gradleProperty("create_version").get()}")
 	implementation("net.fabricmc:fabric-loader:${providers.gradleProperty("loader_version").get()}")
 	implementation("maven.modrinth:jade:${providers.gradleProperty("jade_version").get()}")
+	compileOnly("me.shedaniel:RoughlyEnoughItems-fabric:${providers.gradleProperty("rei_version").get()}")
+	compileOnly("me.shedaniel:RoughlyEnoughItems-api-fabric:${providers.gradleProperty("rei_version").get()}")
+	compileOnly ("me.shedaniel.cloth:cloth-config-fabric:26.1.154")
+	compileOnly ("dev.architectury:architectury-fabric:20.0.7")
 	// Fabric API. This is technically optional, but you probably want it anyway.
 	implementation("net.fabricmc.fabric-api:fabric-api:${providers.gradleProperty("fabric_api_version").get()}")
 	implementation("maven.modrinth:rrv:${providers.gradleProperty("rrv_version").get()}") {
