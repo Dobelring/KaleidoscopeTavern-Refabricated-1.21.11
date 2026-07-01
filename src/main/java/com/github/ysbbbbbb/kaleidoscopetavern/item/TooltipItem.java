c83d530135ec58934ed66bb8c6bd866a4a6ea7a6
NightEpiphany <nightepiphany0401@gmail.com>
2026-07-01 21:07:24 +0800

    文本信息补充

---
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopetavern/item/TooltipItem.java
@@ -0,0 +1,28 @@
+package com.github.ysbbbbbb.kaleidoscopetavern.item;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class TooltipItem extends Item {
+    private final String[] tooltipKeys;
+
+    public TooltipItem(Properties properties, String... tooltipKeys) {
+        super(properties);
+        this.tooltipKeys = tooltipKeys;
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltip, @NotNull TooltipFlag flag) {
+        for (String tooltipKey : this.tooltipKeys) {
+            tooltip.add(Component.translatable(tooltipKey).withStyle(ChatFormatting.GRAY));
+        }
+    }
+}
