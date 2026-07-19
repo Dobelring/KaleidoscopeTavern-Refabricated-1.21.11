a45edeb60313c1e9367488d29fa5b8aa5f2bc489
NightEpiphany <nightepiphany0401@gmail.com>
2026-07-19 20:17:35 +0800

    龙头修复

--- src/main/java/com/github/ysbbbbbb/kaleidoscopetavern/block/brew/TapBlock.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopetavern/block/brew/TapBlock.java
@@ -241,7 +241,11 @@ public class TapBlock extends BaseEntityBlock implements SimpleWaterloggedBlock 
         Direction direction = blockState.getValue(FACING);
         BlockPos blockPosBase = blockPos.relative(direction.getOpposite());
         BlockState blockStateBase = levelReader.getBlockState(blockPosBase);
-        return blockStateBase.isFaceSturdy(levelReader, blockPos, direction) || blockStateBase.is(Blocks.CAULDRON);
+        return
+                blockStateBase.isFaceSturdy(levelReader, blockPos, direction)
+                        || blockStateBase.getBlock() instanceof AbstractCauldronBlock
+                        || blockStateBase.is(Blocks.DRAGON_WALL_HEAD)
+                        || blockStateBase.is(Blocks.DRAGON_HEAD);
     }
 
     @Override
