65075ed4b56ef9d4866d5845e307a81ed035af74
NightEpiphany <nightepiphany0401@gmail.com>
2026-08-09 14:28:28 +0800

    去除DragonLib兼容

--- src/main/java/com/github/ysbbbbbb/kaleidoscopetavern/mixin/plugin/KaleidoscopeTavernMixinPlugin.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopetavern/mixin/plugin/KaleidoscopeTavernMixinPlugin.java
@@ -1,6 +1,5 @@
 package com.github.ysbbbbbb.kaleidoscopetavern.mixin.plugin;
 
-import net.fabricmc.loader.api.FabricLoader;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -9,9 +8,6 @@ import java.util.List;
 import java.util.Set;
 
 public class KaleidoscopeTavernMixinPlugin implements IMixinConfigPlugin {
-    private static final String DRAGONLIB_BLOCK_MODEL_COMPAT =
-            "com.github.ysbbbbbb.kaleidoscopetavern.mixin.compat.dragonlib.DragonLibBlockModelCompatMixin";
-
     @Override
     public void onLoad(String mixinPackage) {
 
@@ -24,10 +20,6 @@ public class KaleidoscopeTavernMixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        if (DRAGONLIB_BLOCK_MODEL_COMPAT.equals(mixinClassName)) {
-            FabricLoader loader = FabricLoader.getInstance();
-            return loader.isModLoaded("dragonlib") && loader.isModLoaded("porting_lib_model_loader");
-        }
         return true;
     }
 
